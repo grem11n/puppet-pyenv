b7b6e560436ee2e9faed176146ed290cfc89cb95
Yurii Rochniak <yrochnyak@gmail.com>
2017-05-30 14:21:21 +0300

    Move check to a separate method

--- lib/puppet/provider/pip/default.rb
+++ lib/puppet/provider/pip/default.rb
@@ -11,9 +11,7 @@ Puppet::Type.type(:pip).provide :default do
     if @resource[:package].kind_of?(Array)
       if @resource[:package_version] and @resource[:package_version].kind_of?(Array)
         @resource[:package].zip(@resource[:package_version]) { |p, v| command << p.to_s + v.to_s }
-      elsif list().detect { |r|
-                            r[:package] == @resource[:package]
-                          }
+      elsif check
         return
       else
         command << @resource[:package]
@@ -45,6 +43,11 @@ Puppet::Type.type(:pip).provide :default do
     end
   end
 
+  def check
+    list().detect { |r|
+      r[:package] == @resource[:package]
+    }
+  end
   def query
     list().detect { |r|
       r[:package] == @resource[:package]
